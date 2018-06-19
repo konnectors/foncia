3833d1deb48dfe0342e800f42c935b44ab296e93
doubleface <christophe@cozycloud.cc>
2018-06-19 12:22:09 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,181 @@
+const {
+  BaseKonnector,
+  requestFactory,
+  signin,
+  scrape,
+  saveBills,
+  log
+} = require('cozy-konnector-libs')
+const request = requestFactory({
+  // the debug mode shows all the details about http request and responses. Very usefull for
+  // debugging but very verbose. That is why it is commented out by default
+  // debug: true,
+  // activates [cheerio](https://cheerio.js.org/) parsing on each page
+  cheerio: true,
+  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
+  // default in cozy-konnector-libs
+  json: false,
+  // this allows request-promise to keep cookies between requests
+  jar: true
+})
+
+module.exports = new BaseKonnector(start)
+
+const vendor = 'foncia'
+const baseUrl = 'https://fr.foncia.com'
+
+async function start(fields) {
+  log('info', 'Authenticating ...')
+  await authenticate(fields.login, fields.password)
+  log('info', 'Successfully logged in')
+
+  log('info', 'Fetching list of properties')
+  const propertiesIds = await getPropertiesIDs()
+  log('info', 'Fetching bills')
+  const bills = await fetchBills(propertiesIds)
+  log('info', 'Saving data to Cozy')
+  await saveBills(bills, fields.folderPath, {
+    identifiers: [vendor]
+  })
+}
+
+async function authenticate(username, password) {
+  const url = `${baseUrl}/login`
+
+  // We need to extract a CSRF token
+  const $ = await request(url)
+  const _csrf_token = $("input[name='_csrf_token']").attr('value')
+
+  return signin({
+    url: `${baseUrl}/login`,
+    formSelector: '.Form',
+    formData: {
+      _username: username,
+      _password: password,
+      _csrf_token
+    },
+    validate: (statusCode, $) => {
+      if ($(`a[href='/logout']`).length === 1) {
+        return true
+      } else {
+        // cozy-konnector-libs has its own logging function which format these
+        // logs with colors in standalone and dev mode and as JSON in production
+        // mode
+        log('error', $('.error').text())
+        return false
+      }
+    }
+  })
+}
+
+// Get the IDs of the different properties one could own/rent.
+async function getPropertiesIDs() {
+  const $ = await request(`${baseUrl}/espace-client/espace-de-gestion/mon-bien`)
+
+  return $('.MyPropertiesSelector-slides li')
+    .map(function(i, el) {
+      return $(el).attr('data-property')
+    })
+    .get()
+}
+
+// Retrieve the "bills" for a given property.
+async function getBillsForProperty(propertyId) {
+  const page = await request(
+    `${baseUrl}/espace-client/espace-de-gestion/mon-bien/${propertyId}`
+  )
+
+  const propertyDesc = page(`span[class="MyPropertiesSelector-item-desc"]`)
+    .first()
+    .text()
+
+  // "Go" to the page where all the documents are accessible.
+  const documentsUrl = page(`a[class='Icon--book']`).attr('href')
+  const $ = await request(`${baseUrl}${documentsUrl}`)
+
+  const bills = scrape(
+    $,
+    {
+      description: {
+        sel: '.TeaserRow-desc'
+      },
+      // The format of the date is: DD.MM.YYYY
+      date: {
+        sel: '.TeaserRow-date',
+        parse: function(text) {
+          if (text !== '') {
+            return normalizeDate(text)
+          } else {
+            return text
+          }
+        }
+      },
+      billPath: {
+        sel: '.Download',
+        attr: 'href'
+      }
+    },
+    '.TeaserRow'
+  )
+
+  return bills.map(bill => ({
+    ...bill,
+    propertyDesc
+  }))
+}
+
+async function fetchBills(propertiesIds) {
+  const bills = []
+  for (let propertyId in propertiesIds) {
+    if (propertyId !== '') {
+      bills.push.apply(bills, await getBillsForProperty(propertyId))
+    }
+  }
+
+  return bills.map(bill => ({
+    ...bill,
+    currency: '€',
+    fileurl: `${baseUrl}${bill.billPath}`,
+    vendor,
+    filename: `${formatFilename(bill)}.pdf`,
+    metadate: {
+      importDate: new Date(),
+      version: 1
+    }
+  }))
+}
+
+// Return a string representation of the date that follows this format:
+// "YYYY-MM-DD". Leading "0" for the day and the month are added if needed.
+function formatDate(date) {
+  let month = date.getMonth() + 1
+  if (month < 10) {
+    month = '0' + month
+  }
+
+  let day = date.getDate()
+  if (day < 10) {
+    day = '0' + day
+  }
+
+  let year = date.getFullYear()
+
+  return `${year}${month}${day}`
+}
+
+// Return a JS Date object from the string representation. The format of the String has
+// to be "DD.MM.YYYY".
+function normalizeDate(date) {
+  let [day, month, year] = date.split('.')
+  return new Date(`${year}-${month}-${day}`)
+}
+
+// Return an appropriate filename given a "bill": some do not have a date
+// associated with it.
+function formatFilename(bill) {
+  if (bill.date === '') {
+    return `${bill.propertyDesc}-${bill.description}`
+  } else {
+    return `${bill.propertyDesc}-${formatDate(bill.date)}-${bill.description}`
+  }
+}
